2cbc6623988bc62fa59f6391b5c59e194d564bb9
Alexandre Miyazaki <alexandre.miyazaki@ifood.com.br>
2022-08-18 08:47:58 -0300

    chore: standardize test names

--- scala/fizzbuzz/src/test/scala/TestFizzBuzz.scala
+++ scala/fizzbuzz/src/test/scala/TestFizzBuzz.scala
@@ -6,19 +6,19 @@ class TestFizzBuzz extends AnyFlatSpec with Matchers with BeforeAndAfter {
   lazy val fizzBuzzHandler = FizzBuzz()
 
 
-  "When the number is not a multiple of 3 or 5" should "return the number in string format" in {
+  "When the number is not a multiple of 3 nor 5" should "return the number in string format" in {
     fizzBuzzHandler.fizzbuzz(1) shouldEqual "1"
   }
 
-  "When the number is multiple of three" should "return Fizz" in {
+  "When the number is multiple of 3" should "return Fizz" in {
     fizzBuzzHandler.fizzbuzz(3) shouldEqual "Fizz"
   }
 
-  "When the number is a multiple of five" should "return Buzz" in {
+  "When the number is a multiple of 5" should "return Buzz" in {
     fizzBuzzHandler.fizzbuzz(5) shouldEqual "Buzz"
   }
 
-  "When the number is a multiple of both three and Five" should "return FizzBuzz" in {
+  "When the number is a multiple of both 3 and 5" should "return FizzBuzz" in {
     fizzBuzzHandler.fizzbuzz(15) shouldEqual "FizzBuzz"
   }
 }
